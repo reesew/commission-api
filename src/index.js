0419563cc7c32fcf52599b6ab415f7a1a4e63777
Reese Wilson <reesew@tzmedical.com>
2015-03-17 13:06:35 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var mysql = require('mysql');
+var app = express();
+
+console.log("Starting commissions api server");
+var server = app.listen(80, function() {
+});
